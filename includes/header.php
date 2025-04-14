92003809982d962c769efe64016e6b913157998b
Luis Lima <luissilvalimaluis@gmail.com>
2025-04-13 23:03:45 -0300

    Ultima atualizacao logo

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,9 @@
         <span>🎶</span>
     </div>
 
-    <a href="/projeto/index.php">Logo de música</a>
+    <a href="/projeto/index.php">
+        <img src="/projeto/assets/img/favico.png">
+    </a>
     <a href="/projeto/index.php">Home</a>
     <a href="/projeto/main/filtro.php">Busca</a>
     <a href="/projeto/main/login.php">Login</a>
