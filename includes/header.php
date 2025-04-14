8ec0e38bc1de51a8eafb9cba2dac7d153817a69b
Luiz Gumz <luiz0augusto0gumz@gmail.com>
2025-04-13 22:15:09 -0600

    Atualização Header

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header>
-    <a href="#">Logo de música</a>
-    <a href="#">Home</a>
-    <a href="#">Álbuns</a>
-    <a href="#">Login</a>
+    <a href="/projeto/index.php">Logo de música</a>
+    <a href="/projeto/index.php">Home</a>
+    <a href="/projeto/filtro.php ">Busca</a>
+    <a href="/projeto/login.php">Login</a>
 </header>
