a3c4a24512479f86ef9bd34a5ef8c42bc4cdc288
Luiz Gumz <luiz0augusto0gumz@gmail.com>
2025-04-13 23:51:29 -0600

    Atualização index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     //Pagina inicial com exibição do catálogo
 
-   
+
     include_once $_SERVER['DOCUMENT_ROOT'].'/projeto/data/itens.php';
     include_once $_SERVER['DOCUMENT_ROOT'].'/projeto/functions/utils.php';
 ?>
@@ -10,10 +10,10 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link rel="icon" type="image/x-icon" href="/assets/img/favico.ico">
     <title>Musicas</title>
+    <link rel="stylesheet" href="./style.css"/> 
     <link rel="stylesheet" href="/projeto/includes/header.css">
     <link rel="stylesheet" href="/projeto/includes/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
@@ -27,7 +27,9 @@
             <div class="col-md-4 mb-4">
                 <div class="card">
 
-                    <img src="<?= $item['imagem'] ?>" class="card-img-top" alt="<?= $item['titulo'] ?>">
+                    <div class="card-img">
+                        <img src="<?= $item['imagem'] ?>" class="card-img-top" alt="<?= $item['titulo'] ?>">
+                    </div>
                     <div class="card-body">
                         <h5 class="card-title"><?= $item['titulo'] ?></h5>
                         <p class="card-text"><?= $item['categoria'] ?></p>
@@ -45,7 +47,3 @@
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/projeto/includes/footer.php'; ?>
 </footer>
 </html>
-
-
-
-
